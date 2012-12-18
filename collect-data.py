c3a28e9d639fb6783d446f22423dad119dc746c2
pronto <pronto185@gmail.com>
2012-12-18 16:09:51 -0500

    yay

--- collect-data.py
+++ collect-data.py
@@ -3,6 +3,15 @@ from ConfigParser import SafeConfigParser
 import glob
 import codecs
 import json
+import os
+import sys
+import re
+import gzip
+import socket
+import GeoIP
+import cPickle
+import shutil
+import operator
 ignore_ip = ['75.101.142.201', '198.101.145.249', '76.21.11.37']
 
 parser =  SafeConfigParser()
@@ -51,3 +60,42 @@ def auth_files():
     else:
         log_list.append("/var/log/"+parser.get("data-collector", "auth_file"))
     return log_list
+
+
+#This next part scans the auth.logs, and tosses them into ip_dict, and if use_geoip is on, it does that to
+#[{'IP':'321.321.321.321','geo':'mars'},{...},{...]
+ip_dict=[]
+for afile in auth_files():
+    try:
+        for line in openfile(afile):
+            if "Failed" in line:
+                ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+                if ip:
+                    try:
+                        index = get_index(ip_dict, 'IP', ip[0])
+                        ip_dict[index]['attempts'] += 1
+                        #if parser.get("data-collector", "use_geoip") ==  "yes":
+                        #    ip_dict[index]['geo'] = gi.record_by_addr(ip[0])['country_name']
+                    except:
+                        ip_dict.append({"IP": ip[0], "attempts": 1})
+    except:
+        print "please run as root....i know running things as root sucks, but auth.log needs it"
+        raise
+        sys.exit()
+
+#remove IPs with less then X amount of attempts
+ip_dict.sort(key=operator.itemgetter('attempts'), reverse=True)
+del_attempts=[]
+for a in ip_dict:
+    if a['attempts'] < int(parser.get("data-collector", "removeresultsunder")):
+        del_attempts.append(get_index(ip_dict,'IP',a['IP']))
+
+del_attempts.sort(reverse=True)
+for a in del_attempts:
+    del ip_dict[a]
+#and GONE!
+
+
+#debug part--- prints out ip_dict
+for a in ip_dict:
+    print a
